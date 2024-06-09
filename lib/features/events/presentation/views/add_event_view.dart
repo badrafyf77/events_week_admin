3bc5a197797e8b2edef0374237b09c706313c40f
badrafyf77 <badr123afyf@gmail.com>
2024-06-09 19:43:29 +0100

    finsh right side UI

--- lib/features/events/presentation/views/add_event_view.dart
+++ lib/features/events/presentation/views/add_event_view.dart
@@ -32,4 +32,3 @@ class AddEventView extends StatelessWidget {
     );
   }
 }
-
--- lib/features/events/presentation/views/widgets/add_event_right_side.dart
+++ lib/features/events/presentation/views/widgets/add_event_right_side.dart
@@ -1,5 +1,6 @@
 import 'package:events_week_admin/core/utils/colors.dart';
 import 'package:events_week_admin/core/utils/customs/button.dart';
+import 'package:events_week_admin/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class AddEventRightSide extends StatelessWidget {
@@ -17,6 +18,35 @@ class AddEventRightSide extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text(
+                  'Image de L\'événement',
+                  style: Styles.normal18,
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         const SizedBox(
