82bdb0e781948a350da0c605bc1177cd1c7ddf7e
badrafyf77 <badr123afyf@gmail.com>
2024-07-16 14:14:03 +0100

    finish gallery ui

--- lib/features/gallery/presentation/views/gallery_view.dart
+++ lib/features/gallery/presentation/views/gallery_view.dart
@@ -1,8 +1,9 @@
+import 'package:events_week_admin/core/utils/assets.dart';
 import 'package:events_week_admin/core/utils/colors.dart';
 import 'package:events_week_admin/core/utils/customs/button.dart';
 import 'package:events_week_admin/core/utils/customs/loading_indicator.dart';
 import 'package:events_week_admin/core/utils/customs/refresh_icon.dart';
-import 'package:events_week_admin/features/activities/presentation/views/widgets/activities_body.dart';
+import 'package:events_week_admin/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class GalleryView extends StatefulWidget {
@@ -49,7 +50,7 @@ class _GalleryViewState extends State<GalleryView> {
                     SizedBox(
                       height: 15,
                     ),
-                    ActivitiesBody(),
+                    GalleryBody(),
                   ],
                 ),
               )
@@ -87,6 +88,133 @@ class GalleryHeader extends StatelessWidget {
               onPressed: () {},
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryBody extends StatelessWidget {
+  const GalleryBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return const GalleryCard();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryCard extends StatelessWidget {
+  const GalleryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        height: 450,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    AppAssets.event,
+                    width: constraints.maxWidth * 0.55,
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Théâtre pour enfants et adolescents",
+                        style: Styles.normal24,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "18/05/2023",
+                        style: Styles.normal16.copyWith(color: Colors.grey),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 8),
+                      const Spacer(),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          EditGallery(),
+                          SizedBox(width: 25),
+                          DeleteGallery(),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class EditGallery extends StatelessWidget {
+  const EditGallery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.edit,
+          size: 22,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class DeleteGallery extends StatelessWidget {
+  const DeleteGallery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.delete,
+          size: 22,
+          color: Colors.white,
         ),
       ),
     );
